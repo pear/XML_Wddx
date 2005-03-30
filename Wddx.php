7fae2858be9d63b792db06b999f816fb88492ca1
Alan Knowles <alan_k@php.net>
2005-03-30 02:43:37 +0000

    prep for bugfix release, and nice new headers..
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_Wddx/trunk@183133 c90b9560-bf6c-de11-be94-00142212c4b1

--- Wddx.php
+++ Wddx.php
@@ -1,34 +1,31 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2004 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.02 of the PHP license,      |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors:  Alan Knowles <alan@akbkhome.com>                           |
-// +----------------------------------------------------------------------+
-//
-//
-
-
 /**
-* XML_Wddx : WDDX serializer and deserializer (works with or without the wddx extension)
-*
-* @abstract
-* serialization is done by   $string = XML_Wddx::serialize($data);
-* deserialization is done by $data   = XML_Wddx::deserialize($string);
-*
-* @version    $Id$
-*/
-
+ * XML_Wddx : WDDX serializer and deserializer (works with or without the wddx extension)
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   XML
+ * @package    XML_Wddx
+ * @author     Alan Knowles <alan@akbkhome.com>
+ * @copyright  1997-2005 The PHP Group
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    $Id$
+ * @link       http://pear.php.net/package/DB_DataObject
+ *
+ *
+ * @abstract
+ * serialization is done by   $string = XML_Wddx::serialize($data);
+ * deserialization is done by $data   = XML_Wddx::deserialize($string);
+ *
+ * 
+ */
+ 
 require_once 'XML/Parser.php';
 
 class XML_Wddx extends XML_Parser {
@@ -144,12 +141,12 @@ class XML_Wddx extends XML_Parser {
                 $this->indent(1);
                 foreach($value as $k=>$v) {
                     $ret .= $this->indent(0);
-                    $ret .= $is_struct ? sprintf("<var name='%s'>",$k) : '<var>';
+                    $ret .= $is_struct ? sprintf("<var name='%s'>",$k) : '';
                     $this->indent(1);
                     $ret .= $this->_serializeValue($v) ;
                     $this->indent(-1);
                     $ret .= ($ret{strlen($ret)-1} == "\n") ? $this->indent() : '';
-                    $ret .= "</var>\n";
+                    $ret .= $is_struct ? "</var>\n" : "\n";
                 }
                 
                 $ret .= $this->indent(-1);
